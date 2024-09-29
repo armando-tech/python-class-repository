ef84f9ded7a9db8d0c239977ce4cd25d0c0481b5
armando-tech <mando.gaona91@gmail.com>
2024-09-29 17:30:56 -0500

    Add files via upload

---
+++ Lesson 1 assignment.py
@@ -0,0 +1,17 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> myname = "Armando"
+>>> myname
+'Armando'
+>>> my name = Armando
+SyntaxError: invalid syntax
+>>> 2 = 5
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+>>> 4 + 10
+14
+>>> 10 == 10
+True
+>>> 10 == 9
+False
+>>> print("Completed by, Armando Gaona")
+Completed by, Armando Gaona
